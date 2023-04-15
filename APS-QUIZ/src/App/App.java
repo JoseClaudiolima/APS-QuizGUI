ecfec6161f8ad061945c9844f14378737dd51727
Samuel <88220934+Samu-Lira@users.noreply.github.com>
2023-04-15 16:55:33 -0300

    Update App.java

--- APS-QUIZ/src/App/App.java
+++ APS-QUIZ/src/App/App.java
@@ -18,44 +18,44 @@ public class App {
         Pontuador pontuador = new Pontuador();
 
         // Em cada uma das questões, mostrará uma interfarce e verificará se o usuário errou ou acertou.
-        String questao1  = JOptionPane.showInputDialog(null, questoes.pergunta1());
+        String questao1  = JOptionPane.showInputDialog(null, questoes.getPergunta1());
         validador.validarResposta("c", questao1);
         
         
-        String questao2  = JOptionPane.showInputDialog(null, questoes.pergunta2());
+        String questao2  = JOptionPane.showInputDialog(null, questoes.getPergunta2());
         validador.validarResposta("a", questao2);
         
 
-        String questao3  = JOptionPane.showInputDialog(null, questoes.pergunta3());
+        String questao3  = JOptionPane.showInputDialog(null, questoes.getPergunta3());
         validador.validarResposta("c", questao3);
       
 
-        String questao4  = JOptionPane.showInputDialog(null, questoes.pergunta4());
+        String questao4  = JOptionPane.showInputDialog(null, questoes.getPergunta4());
         validador.validarResposta("c", questao4);
       
 
-        String questao5  = JOptionPane.showInputDialog(null, questoes.pergunta5());
+        String questao5  = JOptionPane.showInputDialog(null, questoes.getPergunta5());
         validador.validarResposta("d", questao5);
       
 
-        String questao6  = JOptionPane.showInputDialog(null, questoes.pergunta6());
+        String questao6  = JOptionPane.showInputDialog(null, questoes.getPergunta6());
         validador.validarResposta("a", questao6);
         
 
-        String questao7  = JOptionPane.showInputDialog(null, questoes.pergunta7());
+        String questao7  = JOptionPane.showInputDialog(null, questoes.getPergunta7());
         validador.validarResposta("b", questao7);
        
 
-        String questao8  = JOptionPane.showInputDialog(null, questoes.pergunta8());
+        String questao8  = JOptionPane.showInputDialog(null, questoes.getPergunta8());
         validador.validarResposta("d", questao8);
 
-        String questao9  = JOptionPane.showInputDialog(null, questoes.pergunta9());
+        String questao9  = JOptionPane.showInputDialog(null, questoes.getPergunta9());
         validador.validarResposta("b", questao9);
 
-        String questao10  = JOptionPane.showInputDialog(null, questoes.pergunta10());
+        String questao10  = JOptionPane.showInputDialog(null, questoes.getPergunta10());
         validador.validarResposta("a", questao10);
 
-        String questao11  = JOptionPane.showInputDialog(null, questoes.pergunta11());
+        String questao11  = JOptionPane.showInputDialog(null, questoes.getPergunta11());
         validador.validarResposta("c", questao11);
        
 
