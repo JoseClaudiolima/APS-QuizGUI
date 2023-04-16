e33f44b0e5d6d3dd3ea2ad9c12c2405c2f38720a
José Cláudio <jose.c.lima.sp@gmail.com>
2023-04-16 14:22:31 -0300

    As linhas das questões dos quiz foram estruturados, e organizados para o tamanho da janela da interface ser o ideal.

--- APS-QUIZ/src/textos/Questoes.java
+++ APS-QUIZ/src/textos/Questoes.java
@@ -4,75 +4,74 @@ public class Questoes {
 
     // Aqui estao armazenadas todas as perguntas do quiz.
     public String[][] todasPerguntasArray = {
-        /*"c", "a", "c", "c", "d", "a", "b", "d", "b", "a", "c" */
-        {"(QUESTÃO FÁCIL) 1- Jogar lixo na natureza não é nada certo, pois muito do lixo produzido pelo homem demoraria muito tempo para se decompor. O vidro, por exemplo, sabe quanto tempo ele demora para se decompor? \n"
+        {"(QUESTÃO FÁCIL) \n1- Jogar lixo na natureza não é nada certo, pois muito do lixo produzido pelo homem demoraria muito tempo para se decompor.\nO vidro, por exemplo, sabe quanto tempo ele demora para se decompor? \n"
         + "\na) ~1500 anos.\n"
         + "b) ~3000 anos.\n"
         + "c) ~4000 anos.\n"
         +"d) Nenhuma das opções."},
 
         
-        {"(QUESTÃO FÁCIL) 2- Sabia que houve uma conferência da ONU sediada no Rio de Janeiro em 1992 sobre o meio ambiente? Pois é, a Eco – 92, ou Rio – 92, foi um marco no esforço global pelo meio ambiente, e dela nasceu um documento importante cheio de normas ambientais, que foi...\n"
+        {"(QUESTÃO FÁCIL) \n2- Sabia que houve uma conferência da ONU sediada no Rio de Janeiro em 1992 sobre o meio ambiente? Pois é, a Eco – 92, ou Rio – 92, foi\num marco no esforço global pelo meio ambiente, e dela nasceu um documento importante cheio de normas ambientais, que foi...\n"
         + "\na) Agenda 21.\n"
         + "b) O Manifesto Ambiental.\n"
         + "c) Manifesto – 92.\n"
         +"d) Nenhuma das opções."},
 
         
-        {"(QUESTÃO FÁCIL) 3- Mesmo o ser humano estando no topo da cadeia alimentar, é notório que não teríamos chance num duelo mano a mano contra diversos animais selvagens. Muito poderiam facilmente esmagar ou lacerar membros apenas com a força da mordida! Causalidades de lado, qual destes animais você acha que tem a mordida mais forte?\n"
+        {"(QUESTÃO FÁCIL) \n3- Mesmo o ser humano estando no topo da cadeia alimentar, é notório que não teríamos chance num duelo mano a mano contra diversos animais selvagens.\nMuito poderiam facilmente esmagar ou lacerar membros apenas com a força da mordida! Causalidades de lado, qual destes animais você acha que tem a \nmordida mais forte?\n"
         + "\na) Babuíno.\n"
         + "b) Leão.\n"
         + "c) Hiena.\n"
         +"d) Lobo."},
 
         
-        {"(QUESTÃO FÁCIL) 4- Lugar de lixo é no lixo. O descarte inadequado acarreta em complicações diversas, como a contaminação do solo e dos lençóis freáticos, porções de água limpa que percorrem o subsolo, incluindo aquíferos, além de problemas para flora e a fauna, como os recifes de corais e os animais aquáticos que sofrem com a poluição dos mares. Uma ação pode começar de baixo com o simples ato de jogar o seu lixo no lugar adequado, e você sabe que lugar é esse? Quais as cores referentes aos tipos de lixo plásticos, metais, vidros, orgânicos e papéis, respectivamente? \n"   
+        {"(QUESTÃO FÁCIL) \n4- Lugar de lixo é no lixo. O descarte inadequado acarreta em complicações diversas, como a contaminação do solo e dos lençóis freáticos,\n porções de água limpa que percorrem o subsolo, incluindo aquíferos, além de problemas para flora e a fauna, como os recifes de corais e \nos animais aquáticos que sofrem com a poluição dos mares. Uma ação pode começar de baixo com o simples ato de jogar o seu lixo no lugar \nadequado, e você sabe que lugar é esse? \nQuais as cores referentes aos tipos de lixo plásticos, metais, vidros, orgânicos e papéis, respectivamente? \n"   
         + "\na) Vermelho, Amarelo, verde, azul e marrom\n"
         + "b) Amarelo, vermelho, marrom, azul e verde.\n"
         + "c) Vermelho, amarelo, verde, marrom e azul.\n"
         +"d) Marrom, azul, vermelho, amarelo e verde."},
 
         
-        {"(QUESTÃO MÉDIA) 5- Dizem que o efeito estufa é balela... Eu sei, parece bobo achar isso né? Até porque na nossa vizinhança estelar temos um ótimo exemplo de como a Terra pode ficar se nada for feito a respeito. Então, qual seria ele?\n"
+        {"(QUESTÃO MÉDIA) \n5- Dizem que o efeito estufa é balela... Eu sei, parece bobo achar isso né? Até porque na nossa vizinhança estelar temos um ótimo exemplo\n de como a Terra pode ficar se nada for feito a respeito. Então, qual seria ele?\n"
         + "\na) Júpter.\n"
         + "b) Marte.\n"
         + "c) Mercúrio.\n"
         +"d) Vênus."},
 
         
-        {"(QUESTÃO MÉDIA) 6- Ozônio, um gás que você já deve ter ouvido falar por fazer parte de uma camada na atmosfera, e por estar com um buraco cada vez maior, tem grande importância na absorção de raios UV – ultravioleta – danosos aos seres humanos, entre outras complicações. Mas afinal, o que é de fato o ozônio?\n"
+        {"(QUESTÃO MÉDIA) \n6- Ozônio, um gás que você já deve ter ouvido falar por fazer parte de uma camada na atmosfera, e por estar com um buraco cada vez maior,\n tem grande importância na absorção de raios UV – ultravioleta – danosos aos seres humanos, entre outras complicações.\n Mas afinal, o que é de fato o ozônio?\n"
         + "\na) O³ - Oxigênio tri-atômico.\n"
         + "b) H³ - Hidrogênio tri-atômico.\n"
         + "c) CO - Monóxido de Carbono.\n"
         +"d) N02 - Dióxido de Nitrogênio."},
 
         
-        {"(QUESTÃO MÉDIA) 7- Plantas são a base da cadeia alimentar, e por isso é importante entender para cuidar e preservar a flora. As plantas sintetizam sua energia, ou seja, produzem seu próprio alimento, através da captação da luz solar num processo químico. Parte da luz que não é absorvida é refletida na faixa de cor verde, característica comum das folhas. Com isso, qual é o processo químico descrito e o que é responsável pela captação da luz solar?\n"
+        {"(QUESTÃO MÉDIA) \n7- Plantas são a base da cadeia alimentar, e por isso é importante entender para cuidar e preservar a flora. As plantas sintetizam sua energia,\n ou seja, produzem seu próprio alimento, através da captação da luz solar num processo químico. Parte da luz que não é absorvida é refletida\n na faixa de cor verde, característica comum das folhas.\n Com isso, qual é o processo químico descrito e o que é responsável pela captação da luz solar?\n"
         + "\na) Respiração e mitocôndria.\n"
         + "b) Fotossíntese e clorofila. \n"
         + "c) Síntese de proteínas e lipídeos.\n"
         +"d) Nenhuma das anteriores."},
     
         
-        {"(QUESTÃO MÉDIA) 8- Todos nós contribuímos indiretamente ou diretamente pra o efeito estufa, seja assistindo televisão, tomando banho, comendo um pacote de biscoito (bolacha?), andando de carro... e por ai vai. Todas essas atividades envolveram ou envolvem de alguma forma alguma ação que agride o meio ambiente, como o uso de combustíveis fósseis ou a poluição de lençóis freáticos, apenas consumindo. Para ajudar a conscientizar as pessoas em todas as feras, criou-se um termo, que nos diz qual o nosso impacto no ambiental:\n"
+        {"(QUESTÃO MÉDIA) \n8- Todos nós contribuímos indiretamente ou diretamente pra o efeito estufa, seja assistindo televisão, tomando banho, comendo um pacote\n de biscoito (bolacha?), andando de carro... e por ai vai. Todas essas atividades envolveram ou envolvem de alguma forma alguma ação que\n agride o meio ambiente, como o uso de combustíveis fósseis ou a poluição de lençóis freáticos, apenas consumindo.\n Para ajudar a conscientizar as pessoas em todas as feras, criou-se um termo, que nos diz qual o nosso impacto no ambiental:\n"
         + "\na) Carbono zero.\n"
         + "b) Sustentabilidade.\n"
         + "c) Aspecto do carbono.\n"
         +"d) Pegada de carbono."},
 
-        {"(QUESTÃO MÉDIA) 9- É de grande urgência que a humanidade trilhe um caminho mais limpo ao tratar de fontes energéticas. Opções não faltam! Dentre as que estão abaixo, qual é a fonte de energia renovável considerada a mais limpa?\n"
+        {"(QUESTÃO MÉDIA) \n9- É de grande urgência que a humanidade trilhe um caminho mais limpo ao tratar de fontes energéticas. Opções não faltam!\n Dentre as que estão abaixo, qual é a fonte de energia renovável considerada a mais limpa?\n"
         + "\na) Eólica.\n"
         + "b) Solar.\n"
         + "c) Biomassa.\n"
         +"d) Hidrelétrica."},
 
-        {"(QUESTÃO DIFÍCIL) 10- O efeito estufa, fenômeno de aquecimento acelerado da atmosfera terrestre, possui seus vilanescos causadores, os GEEs, ou gases do efeito estufa. O maior contribuinte nasce do uso de combustíveis fósseis e atividade industrial, mas não é necessariamente o mais potente. O segundo maior contribuinte é mais potente e advém principalmente da extração de petróleo, carvão e atividade pecuária com uso de animais ruminantes. O terceiro maior culpado é ainda menos contribuinte e mais potente, fruto da atividade agrícola com uso de agrotóxicos e químicos no cultivo. Dadas as informações, quais são esses 3 GEEs, respectivamente?\n"
+        {"(QUESTÃO DIFÍCIL) \n10- O efeito estufa, fenômeno de aquecimento acelerado da atmosfera terrestre, possui seus vilanescos causadores, os GEEs, ou gases do efeito estufa.\n O maior contribuinte nasce do uso de combustíveis fósseis e atividade industrial, mas não é necessariamente o mais potente.\n O segundo maior contribuinte é mais potente e advém principalmente da extração de petróleo, carvão e atividade pecuária com uso de animais ruminantes.\n O terceiro maior culpado é ainda menos contribuinte e mais potente, fruto da atividade agrícola com uso de agrotóxicos e químicos no cultivo.\n Dadas as informações, quais são esses 3 GEEs, respectivamente?\n"
         + "\na) CO2 - Dióxido de Carbono; CH4 - Metano; N2O - Óxido Nitroso.\n"
         + "b) H2O - Água; O3 - Ozônio; CO - Monóxido de Carbono.\n"
         + "c) SO2 - Dióxido de Enxofre; NH3 - Amônia; NOx - Óxidos de Nitrogênio.\n"
         +"d) VOCs - Compostos Orgânicos Voláteis; HFCs - Hidrofluorocarbonetos; H2S - Sulfeto de Hidrogênio."},
 
-        {"(QUESTÃO DIFÍCIL) 11- Qual o nome do processo que descreve um escoamento de água na camada superficial do solo, causado por intensas chuvas, que transporta os nutrientes do solo a áreas mais baixas, levando ao empobrecimento da terra? \n"
+        {"(QUESTÃO DIFÍCIL) \n11- Qual o nome do processo que descreve um escoamento de água na camada superficial do solo, causado por intensas chuvas, que transporta\n os nutrientes do solo a áreas mais baixas, levando ao empobrecimento da terra? \n"
         + "\na) Osmose.\n"
         + "b) Infiltração.\n"
         + "c) Lixiviação.\n"
